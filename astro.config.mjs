e40796196729d7589e8b63361d06261a1208a0c6
Alan Navarrete <ing.alan.navarrete.sa@gmail.com>
2026-03-23 22:46:37 -0600

    Ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
+  output:'static',
   vite: {
     plugins: [tailwindcss()]
   }
